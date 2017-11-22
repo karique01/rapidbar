30b5ce22d35842b17c34599788221f2b61d8daa3
Sofia Castillo <sofia19castle@gmail.com>
2017-11-22 17:29:48 -0500

    BarmanVistaCompletado
    fragment barman
    card_order:barman ,etc

---
+++ app/src/main/java/pe/edu/upc/rapidbar/adapters/OrderBarmanAdapter.java
@@ -0,0 +1,95 @@
+package pe.edu.upc.rapidbar.adapters;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pe.edu.upc.rapidbar.R;
+import pe.edu.upc.rapidbar.models.Order;
+
+import static android.support.constraint.R.id.parent;
+
+
+public class OrderBarmanAdapter extends RecyclerView.Adapter<OrderBarmanAdapter.OrderBarmanViewHolder> {
+
+    private Context context;
+
+    private List<Order> orders;
+
+
+    public OrderBarmanAdapter() {
+    }
+
+    public OrderBarmanAdapter(List<Order> orders) {
+        //this.context = context;
+        this.orders = orders;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public List<Order> setOrders(List<Order> orders) {
+        this.orders = orders;
+        return orders;
+    }
+
+    @Override
+    public OrderBarmanAdapter.OrderBarmanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_order_barman, parent, false);
+
+        return new OrderBarmanAdapter.OrderBarmanViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(OrderBarmanAdapter.OrderBarmanViewHolder holder, int position) {
+        final Order order = orders.get(position);
+        holder.name.setText("Orden número: " + order.getId());
+
+        holder.price.setText("Precio: S/." + order.getTotalAmount());
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+    public void removeItem(int position) {
+        orders.remove(position);
+        // notify the item removed by position
+        // to perform recycler view delete animations
+        // NOTE: don't call notifyDataSetChanged()
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(Order order, int position) {
+        orders.add(position, order);
+        // notify item added by position
+        notifyItemInserted(position);
+    }
+
+    public class OrderBarmanViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, price,seemore;
+        public ImageView thumbnail;
+        public RelativeLayout viewBackground, viewForeground;
+
+        public OrderBarmanViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.orderName);
+            price = view.findViewById(R.id.price);
+            thumbnail = view.findViewById(R.id.thumbnail);
+            seemore = view.findViewById(R.id.orderBarmanSeeMore);
+            viewBackground = view.findViewById(R.id.view_background);
+            viewForeground = view.findViewById(R.id.view_foreground);
+        }
+    }
+
+}
+
--- app/src/main/java/pe/edu/upc/rapidbar/fragments/BarmanFragment.java
+++ app/src/main/java/pe/edu/upc/rapidbar/fragments/BarmanFragment.java
@@ -1,30 +1,133 @@
 package pe.edu.upc.rapidbar.fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import pe.edu.upc.rapidbar.R;
+import pe.edu.upc.rapidbar.adapters.OrderBarmanAdapter;
+import pe.edu.upc.rapidbar.adapters.OrderChefAdapter;
+import pe.edu.upc.rapidbar.helpers.ChefRecyclerTouchHelper;
+import pe.edu.upc.rapidbar.models.Order;
+import pe.edu.upc.rapidbar.network.RapidBarApiService;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BarmanFragment extends Fragment {
+public class BarmanFragment extends Fragment  implements ChefRecyclerTouchHelper.ChefRecyclerTouchHelperListener {
+
+    RecyclerView ordersBarmanRecyclerView;
+    OrderChefAdapter ordersBarmanAdapter;
+    RecyclerView.LayoutManager ordersBarmanLayoutManager;
+    List<Order>orders;
 
 
     public BarmanFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_barman, container, false);
+        updateData();
+        View view = inflater.inflate(R.layout.fragment_barman, container, false);
+
+        //RecyclerView Setup
+        ordersBarmanRecyclerView = (RecyclerView) view.findViewById(R.id.barmanRecyclerView);
+        orders = new ArrayList<>();
+
+        ordersBarmanAdapter = new OrderChefAdapter(orders);
+
+        ordersBarmanLayoutManager = new LinearLayoutManager(view.getContext());
+        ordersBarmanRecyclerView.setLayoutManager(ordersBarmanLayoutManager);
+
+        ordersBarmanRecyclerView.setAdapter(ordersBarmanAdapter);
+
+        ordersBarmanRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        ordersBarmanRecyclerView.addItemDecoration(new DividerItemDecoration(this.getContext(), DividerItemDecoration.VERTICAL));
+
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ChefRecyclerTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(ordersBarmanRecyclerView);
+
+        return view;
+
     }
 
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+
+        if (viewHolder instanceof OrderBarmanAdapter.OrderBarmanViewHolder) {
+            // get the removed item name to display it in snack bar
+            String name = orders.get(viewHolder.getAdapterPosition()).getId();
+
+            // backup of removed item for undo purpose
+            final Order deletedItem = orders.get(viewHolder.getAdapterPosition());
+            final int deletedIndex = viewHolder.getAdapterPosition();
+
+            // remove the item from recycler view
+            ordersBarmanAdapter.removeItem(viewHolder.getAdapterPosition());
+
+            // showing snack bar with Undo option
+            Snackbar snackbar = Snackbar
+                    .make(this.getView(), "Orden N: " + name + " completada!", Snackbar.LENGTH_LONG);
+            snackbar.setAction("DESHACER", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // undo is selected, restore the deleted item
+                    ordersBarmanAdapter.restoreItem(deletedItem, deletedIndex);
+                }
+            });
+            snackbar.setActionTextColor(Color.RED);
+            snackbar.show();
+        }
+    }
+
+    private void updateData() {
+        AndroidNetworking.get(RapidBarApiService.ORDERS_URL)
+                .setPriority(Priority.HIGH.LOW)
+                .setTag(getString(R.string.app_name))
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            orders = Order.from(response);
+                            ordersBarmanAdapter.setOrders(orders);
+                            ordersBarmanAdapter.notifyDataSetChanged();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+
+                    }
+                });
+
+    }
 }
+
+
+
+
